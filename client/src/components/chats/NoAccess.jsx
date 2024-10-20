0d34005f7ddf34cd930b34045e448c26f9cc9ec7
sumitst05 <sumitst05@gmail.com>
2024-10-20 20:00:05 +0530

    added: vercel.json

--- client/src/components/chats/NoAccess.jsx
+++ client/src/components/chats/NoAccess.jsx
@@ -1,4 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+
 export const NoAccess = ({ roomName }) => {
+  const navigate = useNavigate();
+  const { role } = useAuth();
+
   return (
     <>
       <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
@@ -8,7 +14,10 @@ export const NoAccess = ({ roomName }) => {
               <h1 className="text-gray-800 font-bold text-2xl">
                 Looks like you don't have access to {roomName} room!
               </h1>
-              <button className="sm:w-full lg:w-auto my-2 border rounded md py-2 px-4 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+              <button
+                className="sm:w-full lg:w-auto my-2 border rounded md py-2 px-4 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
+                onClick={() => navigate(`/chat/${role.toLowerCase()}`)}
+              >
                 Take me to my room!
               </button>
             </div>

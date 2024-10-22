c2248f67c565b692332754fc03464ee796c1ac1d
sumitst05 <sumitst05@gmail.com>
2024-10-22 14:52:11 +0530

    chore: modularized websockets code; feat: started with message reply handling
        pick 78bc51c chore: modularized websockets code; feat: started with message reply handling

--- server/websocket.js
+++ server/websocket.js
@@ -1,9 +1,12 @@
 import { Server } from "socket.io";
-import Message from "./models/message.js";
 import { authSocket } from "./middlewares/authSocket.js";
+import { joinRoom } from "./controllers/room.js";
+import { fetchMessages, sendMessage } from "./controllers/message.js";
+
+let io;
 
 export const handleWebsocket = (httpServer) => {
-	const io = new Server(httpServer, {
+	io = new Server(httpServer, {
 		cors: {
 			origin: [
 				"http://localhost:5173",
@@ -17,47 +20,10 @@ export const handleWebsocket = (httpServer) => {
 	io.use(authSocket);
 
 	io.on("connection", (socket) => {
-		socket.on("joinRoom", (room) => {
-			socket.join(room);
-		});
-
-		socket.on("sendMessage", async (messageData) => {
-			const { content, contentType, channelType, replyTo, mentions } =
-				messageData;
-
-			try {
-				const message = await Message.create({
-					content,
-					contentType,
-					sender: socket.user.id,
-					channelType,
-					replyTo,
-					mentions,
-				});
-
-				const messageWithUser = await Message.findById({
-					_id: message._id,
-				}).populate({
-					path: "sender",
-					select: "profilePicture username",
-				});
-
-				io.to(channelType).emit("newMessage", messageWithUser);
-			} catch (error) {
-				console.log("Error saving message:", error.message);
-			}
-		});
-
-		socket.on("fetchMessages", async (room) => {
-			try {
-				const messages = await Message.find({ channelType: room }).populate({
-					path: "sender",
-					select: "profilePicture username",
-				});
-				socket.emit("messages", messages);
-			} catch (error) {
-				console.error("Error fetching messages:", error);
-			}
-		});
+		socket.on("joinRoom", (room) => joinRoom(socket, room));
+		socket.on("fetchMessages", (room) => fetchMessages(socket, room));
+		socket.on("sendMessage", (messageData) => sendMessage(socket, messageData));
 	});
 };
+
+export const getIo = () => io;
